6ca7f1d8d731bcfd52162fbbc90ccce31e394d5e
samira1980dev <samira1980dev@gmail.com>
2022-05-26 21:46:29 -0300

    primeiro teste

---
+++ todo_list/src/todoCollection.ts
@@ -0,0 +1,29 @@
+import { TodoItem } from "./TodoItem";
+
+export class TodoCollection {
+  private nextId: number = 1;
+
+  constructor(public userName: string, public todoItems: TodoItem[] = []) {
+    // no statments required
+  }
+
+  addTodo(task: string): number {
+    while (this.getTodoById(this.nextId)) {
+      this.nextId++;
+    }
+    this.todoItems.push(new TodoItem(this.nextId, task));
+    return this.nextId;
+  }
+
+  getTodoById(id: number): TodoItem {
+    return this.todoItems.find(item => item.id === id);
+  }
+
+  markComplete(id: number, complete: boolean) {
+    const TodoItem = this.getTodoById(id);
+    if(todoItem) {
+      todoItem.complete = complete;
+    }
+
+  }
+}
